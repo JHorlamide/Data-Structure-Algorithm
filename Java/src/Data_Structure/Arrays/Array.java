6065cf6d374a1f63d1487211147ea333acab3ea5
olamide jubril <olamidejubril68@gmail.com>
2024-04-23 21:21:34 +0100

    Formatted code properly.

--- Java/src/Data_Structure/Arrays/Array.java
+++ Java/src/Data_Structure/Arrays/Array.java
@@ -4,160 +4,154 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class Array {
-    private int[] arrayItems;
-    private int insertCount;
+   private int[] arrayItems;
+   private int insertCount;
 
-    public Array(int length) {
-        arrayItems = new int[length];
-    }
+   public Array(int length) {
+      arrayItems = new int[length];
+   }
 
-    public void insert(int item) {
-        //If the array is full, resize it.
-        if (arrayItems.length == insertCount) {
-            // Create a new array
-            int[] newArrayBucket = new int[insertCount * 2];
+   public void insert(int item) {
+      //If the array is full, resize it.
+      if (arrayItems.length == insertCount) {
+         // Create a new array
+         int[] newArrayBucket = new int[insertCount * 2];
 
-            // copy all exiting arrayItems to new array.
-            for (int i = 0; i < insertCount; i++) {
-                newArrayBucket[i] = arrayItems[i];
-            }
+         // copy all exiting arrayItems to new array.
+         System.arraycopy(arrayItems, 0, newArrayBucket, 0, insertCount);
 
-            // Set "arrayItems" to newArrayItems
-            arrayItems = newArrayBucket;
-        }
+         // Set "arrayItems" to newArrayItems
+         arrayItems = newArrayBucket;
+      }
 
-        // Add the new items at the end
-        this.arrayItems[insertCount++] = item;
-    }
+      // Add the new items at the end
+      this.arrayItems[insertCount++] = item;
+   }
 
-    public void print() {
-        for (int i = 0; i < insertCount; i++) {
-            System.out.println(arrayItems[i]);
-        }
-    }
+   public void print() {
+      for (int i = 0; i < insertCount; i++) {
+         System.out.println(arrayItems[i]);
+      }
+   }
 
-    public void removeAt(int index) {
-        // Validate the given index
-        if (index < 0 || index >= insertCount) {
-            throw new IllegalArgumentException();
-        }
+   public void removeAt(int index) {
+      // Validate the given index
+      if (index < 0 || index >= insertCount) {
+         throw new IllegalArgumentException();
+      }
 
-        //Shift the remaining items to the left to fill the hole
-        // Example: [10, 20, 30, 40];
-        // Given Index: 1;
-        // 1 <- 2
-        // 2 <- 3
-        for (int i = index; i < insertCount; i++) {
-            arrayItems[i] = arrayItems[i + 1];
-        }
+      //Shift the remaining items to the left to fill the hole
+      // Example: [10, 20, 30, 40];
+      // Given Index: 1;
+      // 1 <- 2
+      // 2 <- 3
+      for (int i = index; i < insertCount; i++) {
+         arrayItems[i] = arrayItems[i + 1];
+      }
 
-        insertCount--;
-    }
+      insertCount--;
+   }
 
-    public int indexOf(int item) {
-        // Loop through the item, if we find the item, we return the index
-        for (int i = 0; i < insertCount; i++) {
-            if (arrayItems[i] == item) {
-                return i;
-            }
-        }
+   public int indexOf(int item) {
+      // Loop through the item, if we find the item, we return the index
+      for (int i = 0; i < insertCount; i++) {
+         if (arrayItems[i] == item) {
+            return i;
+         }
+      }
 
-        // else we return  -1
-        return -1;
-    }
+      // else we return  -1
+      return -1;
+   }
 
-    public int maxValue() { // Runtime Complexity is O(n)
-        int max = arrayItems[0];
+   public int maxValue() { // Runtime Complexity is O(n)
+      int max = arrayItems[0];
 
-        for (int i = 1; i < insertCount; i++) {
-            if (arrayItems[i] > max) {
-                max = arrayItems[i];
-            }
-        }
+      for (int i = 1; i < insertCount; i++) {
+         if (arrayItems[i] > max) {
+            max = arrayItems[i];
+         }
+      }
 
-        return max;
+      return max;
 
-        //return Data_Structure.Arrays.stream(arrayItems).max().getAsInt();
-    }
+      //return Data_Structure.Arrays.stream(arrayItems).max().getAsInt();
+   }
 
-    public void getCommonItem(Array newArray) {
-        HashSet<Integer> set = new HashSet<>();
+   public void getCommonItem(Array newArray) {
+      HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < insertCount; i++) {
-            set.add(arrayItems[i]);
-        }
+      for (int i = 0; i < insertCount; i++) {
+         set.add(arrayItems[i]);
+      }
 
-        for (int j : newArray.arrayItems) {
-            if (set.contains(j)) {
-                System.out.println(j + " ");
-            }
-        }
-    }
+      for (int j : newArray.arrayItems) {
+         if (set.contains(j)) {
+            System.out.println(j + " ");
+         }
+      }
+   }
 
-    public int[] reverse() {
-        int[] newItems = new int[insertCount];
-        int count = 0;
+   public int[] reverse() {
+      int[] newItems = new int[insertCount];
+      int count = 0;
 
-        for (int i = insertCount; i > 0; i--) {
-            newItems[count++] = arrayItems[i - 1];
-        }
+      for (int i = insertCount; i > 0; i--) {
+         newItems[count++] = arrayItems[i - 1];
+      }
 
-        return newItems;
-    }
+      return newItems;
+   }
 
-    public boolean hasTwoCommonItems() {
-        HashSet<Integer> set = new HashSet<>();
+   public boolean hasTwoCommonItems() {
+      HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < insertCount; i++) {
-            if (set.contains(arrayItems[i])) {
-                return true;
-            }
+      for (int i = 0; i < insertCount; i++) {
+         if (set.contains(arrayItems[i])) {
+            return true;
+         }
 
-            set.add(arrayItems[i]);
-        }
+         set.add(arrayItems[i]);
+      }
 
-        return false;
-    }
+      return false;
+   }
 
-    public void insertAt(int item, int index) {
-        // item => 6
-        // index => 1
-        // [1, 2, 3, 4, 5]
-        // [1, 6, 2, 3, 4, 5]
+   public void insertAt(int item, int index) {
+      // item => 6
+      // index => 1
+      // [1, 2, 3, 4, 5]
+      // [1, 6, 2, 3, 4, 5]
 
-        // validate the index
-        if (index < 0 || index > insertCount) {
-            throw new IllegalArgumentException();
-        }
+      // validate the index
+      if (index < 0 || index > insertCount) {
+         throw new IllegalArgumentException();
+      }
 
-        // Copy the array items starting from the insertion index to the last item;
-        int[] copiedArrayItems = new int[insertCount - 1];
+      // Copy the array items starting from the insertion index to the last item;
+      int[] copiedArrayItems = new int[insertCount - 1];
 
-        for (int i = index; i < insertCount; i++) {
-            copiedArrayItems[i - 1] = arrayItems[i];
-        }
+      System.arraycopy(arrayItems, index, copiedArrayItems, index - 1, insertCount - index);
 
-        System.out.println("CopiedArrayItems: " + Arrays.toString(copiedArrayItems));
-        // Increase the size of the array
-        int[] newItems = new int[insertCount + index];
+      System.out.println("CopiedArrayItems: " + Arrays.toString(copiedArrayItems));
+      // Increase the size of the array
+      int[] newItems = new int[insertCount + index];
 
-        for (int i = 0; i < insertCount; i++) {
-            newItems[i] = arrayItems[i];
-        }
+      if (insertCount >= 0) System.arraycopy(arrayItems, 0, newItems, 0, insertCount);
 
-        arrayItems = newItems;
-        System.out.println("Increased arrayItem size: " + Arrays.toString(arrayItems));
+      arrayItems = newItems;
+      System.out.println("Increased arrayItem size: " + Arrays.toString(arrayItems));
 
-        // Insert the item at the given index, and insert the copied items.
-        for (int i = 0; i < copiedArrayItems.length; i++) {
-            if (i == index) {
-                arrayItems[index] = item;
-                continue;
-            }
+      // Insert the item at the given index, and insert the copied items.
+      for (int i = 0; i < copiedArrayItems.length; i++) {
+         if (i == index) {
+            arrayItems[index] = item;
+            continue;
+         }
 
-            arrayItems[index + i] = copiedArrayItems[i];
-        }
+         arrayItems[index + i] = copiedArrayItems[i];
+      }
 
-        System.out.println("Updated arrayItem: " + Arrays.toString(arrayItems));
-    }
+      System.out.println("Updated arrayItem: " + Arrays.toString(arrayItems));
+   }
 }
--- Java/src/Data_Structure/LinkedList/LinkedList.java
+++ Java/src/Data_Structure/LinkedList/LinkedList.java
@@ -3,199 +3,199 @@ package Data_Structure.LinkedList;
 import java.util.NoSuchElementException;
 
 public class LinkedList {
-    private final int NOT_FOUND = -1;
-    private Node head; // first
-    private Node tail; // last
-    private int size;
+   private final int NOT_FOUND = -1;
+   private Node head; // first
+   private Node tail; // last
+   private int size;
 
-    // addLast
-    public void append(int item) {
-        var node = new Node(item);
+   // addLast
+   public void append(int item) {
+      var node = new Node(item);
 
-        if (isEmpty()) {
-            head = tail = node;
-        } else {
-            tail.next = node;
-            tail = node;
-        }
+      if (isEmpty()) {
+         head = tail = node;
+      } else {
+         tail.next = node;
+         tail = node;
+      }
 
-        size++;
-    }
+      size++;
+   }
 
-    // addFirst
-    public void prepend(int item) {
-        var node = new Node(item);
+   // addFirst
+   public void prepend(int item) {
+      var node = new Node(item);
 
-        if (isEmpty()) {
-            head = tail = node;
-        } else {
-            node.next = head;
-            head = node;
-        }
+      if (isEmpty()) {
+         head = tail = node;
+      } else {
+         node.next = head;
+         head = node;
+      }
 
-        size++;
-    }
+      size++;
+   }
 
-    public int indexOf(int item) {
-        int index = 0;
-        Node current = head;
+   public int indexOf(int item) {
+      int index = 0;
+      Node current = head;
 
-        while (current != null) {
-            if (current.value == item) {
-                return index;
-            }
+      while (current != null) {
+         if (current.value == item) {
+            return index;
+         }
 
-            current = current.next;
-            index++;
-        }
+         current = current.next;
+         index++;
+      }
 
-        return NOT_FOUND;
-    }
+      return NOT_FOUND;
+   }
 
-    public boolean contains(int item) {
-        return indexOf(item) != NOT_FOUND;
-    }
+   public boolean contains(int item) {
+      return indexOf(item) != NOT_FOUND;
+   }
 
-    public void removeFist() {
-        if (isEmpty())
-            return;
+   public void removeFist() {
+      if (isEmpty())
+         return;
 
-        if (head == tail) {
-            head = tail = null;
-        } else {
-            var second = head.next;
-            head.next = null;
-            head = second;
-        }
+      if (head == tail) {
+         head = tail = null;
+      } else {
+         var second = head.next;
+         head.next = null;
+         head = second;
+      }
 
-        size--;
-    }
+      size--;
+   }
 
-    public void removeLast() {
-        // [10 -> 20 -> 30]
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
+   public void removeLast() {
+      // [10 -> 20 -> 30]
+      if (isEmpty()) {
+         throw new NoSuchElementException();
+      }
 
-        if (head == tail) {
-            head = tail = null;
-        } else {
-            var previous = getPreviousNode(tail);
-            tail = previous;
-            tail.next = null;
-        }
+      if (head == tail) {
+         head = tail = null;
+      } else {
+         var previous = getPreviousNode(tail);
+         tail = previous;
+         tail.next = null;
+      }
 
-        size--;
-    }
+      size--;
+   }
 
-    public int size() {
-        return this.size;
-    }
+   public int size() {
+      return this.size;
+   }
 
-    public int[] toArray() {
-        int[] array = new int[size];
-        Node current = head;
-        int index = 0;
+   public int[] toArray() {
+      int[] array = new int[size];
+      Node current = head;
+      int index = 0;
 
-        while (current != null) {
-            array[index++] = current.value;
-            current = current.next;
-        }
+      while (current != null) {
+         array[index++] = current.value;
+         current = current.next;
+      }
 
-        return array;
-    }
+      return array;
+   }
 
-    public void reverse() {
-        if (isEmpty()) {
-            return;
-        }
+   public void reverse() {
+      if (isEmpty()) {
+         return;
+      }
 
-        // [10 -> 20 -> 30] = reversed = [30 -> 20 -> 10]
-        /* First Approach */
-        Node current = head;
-        Node previous = null;
-        Node next = null;
+      // [10 -> 20 -> 30] = reversed = [30 -> 20 -> 10]
+      /* First Approach */
+      Node current = head;
+      Node previous = null;
+      Node next = null;
 
-        while (current != null) {
-            next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
-        }
+      while (current != null) {
+         next = current.next;
+         current.next = previous;
+         previous = current;
+         current = next;
+      }
 
-        head = previous;
-    }
+      head = previous;
+   }
 
-    public void reverse2() {
-        if (isEmpty()) {
-            return;
-        }
+   public void reverse2() {
+      if (isEmpty()) {
+         return;
+      }
 
-        /* Second Approach */
-        // [10 -> 20 -> 30] = reversed = [30 <- 20 <- 10]
-        Node current = head;
-        Node second = current.next;
+      /* Second Approach */
+      // [10 -> 20 -> 30] = reversed = [30 <- 20 <- 10]
+      Node current = head;
+      Node second = current.next;
 
-        while (second != null) {
-            var temp = second.next;
-            second.next = current;
-            head = second;
-            second = temp;
-        }
+      while (second != null) {
+         var temp = second.next;
+         second.next = current;
+         head = second;
+         second = temp;
+      }
 
-        head.next = null;
-        head = current;
-    }
+      head.next = null;
+      head = current;
+   }
 
-    public Node getKthNodeFromTheEnd(int k) {
-        // [10 -> 20 -> 30 -> 40 -> 50]
-        // k = 1 => 50
-        // k = 2 => 40
-        // k = 3 => 30
+   public Node getKthNodeFromTheEnd(int k) {
+      // [10 -> 20 -> 30 -> 40 -> 50]
+      // k = 1 => 50
+      // k = 2 => 40
+      // k = 3 => 30
 
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
+      if (isEmpty()) {
+         throw new NoSuchElementException();
+      }
 
-        if (k < 0 || k == 0) {
-            throw new IllegalArgumentException();
-        }
+      if (k < 0 || k == 0) {
+         throw new IllegalArgumentException();
+      }
 
-        Node currentNode = head;
-        int count = 0;
+      Node currentNode = head;
+      int count = 0;
 
-        while (count != size - k) {
-            currentNode = currentNode.next;
-            count++;
-        }
+      while (count != size - k) {
+         currentNode = currentNode.next;
+         count++;
+      }
 
-        return currentNode;
-    }
+      return currentNode;
+   }
 
-    private Node getPreviousNode(Node node) {
-        Node current = head;
+   private Node getPreviousNode(Node node) {
+      Node current = head;
 
-        while (current != null) {
-            if (current.next == node) {
-                return current;
-            }
+      while (current != null) {
+         if (current.next == node) {
+            return current;
+         }
 
-            current = current.next;
-        }
+         current = current.next;
+      }
 
-        return null;
-    }
+      return null;
+   }
 
-    private boolean isEmpty() {
-        return head == null;
-    }
+   private boolean isEmpty() {
+      return head == null;
+   }
 
-    private class Node {
-        private int value;
-        private Node next;
+   private class Node {
+      private int value;
+      private Node next;
 
-        public Node(int value) {
-            this.value = value;
-        }
-    }
+      public Node(int value) {
+         this.value = value;
+      }
+   }
 }
